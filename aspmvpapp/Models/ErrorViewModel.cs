3f94030f6a12c7de503169d621b7c00c63940260
kind00m <kin4web@gmail.com>
2021-03-16 18:30:59 +0200

    common template

---
+++ aspmvpapp/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace aspmvpapp.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
